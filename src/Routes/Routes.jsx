a0c4e05ddc8b6a71dfbb3d953e9474b15ac034a7
Mashruf Mahmud <mashruf.mahmud@g.bracu.ac.bd>
2024-04-30 02:22:29 +0600

    login,register done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,17 +2,27 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import Main from "../Layout/Main";
-// import Home from "../Pages/Home/Home/Home";
-  
+import Home from "../Pages/Home/Home/Home";
+import Login from "../Pages/Auth/Login/Login";
+import Register from "../Pages/Auth/Register/Register";
+
  export const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
       children: [
-        // {
-        //   path: "/",
-        //   element: <Home></Home>,
-        // }
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+        {
+          path: 'login',
+          element: <Login></Login>,
+        },
+        {
+          path: 'register',
+          element: <Register></Register>,
+        }, 
       ]
     },
   ]);
