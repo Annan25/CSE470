2011b38a6a4fa28feca6bd3a2d0162651de8ca4e
Annan25 <afra.annan@g.bracu.ac.bd>
2024-04-30 12:05:28 +0600

    carparts, rental system, car dealership and details page added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,10 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Auth/Login/Login";
 import Register from "../Pages/Auth/Register/Register";
+import CarParts from "../Pages/CarParts/CarParts";
+import RentalSystem from "../Pages/RentalSystem/RentalSystem";
+import CarDealership from "../Pages/CarDealership/CarDealership";
+import CarDealershipDetails from "../Pages/CarDealership/CarDealershipDetails";
 
  export const router = createBrowserRouter([
     {
@@ -22,7 +26,27 @@ import Register from "../Pages/Auth/Register/Register";
         {
           path: 'register',
           element: <Register></Register>,
-        }, 
+        },
+        {
+          path: "RentalSystem",
+          element: <RentalSystem></RentalSystem>,
+          loader: () =>  fetch("http://localhost:5000/destinations"),
+        },
+        {
+          path: "carParts",
+          element: <CarParts></CarParts>,
+          loader: () =>  fetch("http://localhost:5000/carParts"),
+        },
+        {
+          path: "cars",
+          element: <CarDealership></CarDealership>,
+          loader: () =>  fetch("http://localhost:5000/sellingCars"),
+        },
+        {
+          path: "cars/:carId",
+          element: <CarDealershipDetails></CarDealershipDetails>,
+          loader: ({params}) =>  fetch(`http://localhost:5000/sellingCars/${params.carId}`),
+        },
       ]
     },
   ]);
